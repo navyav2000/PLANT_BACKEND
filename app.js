8118b3ccd9101c5dd1d55679ed1299f675a46848
navya2000v <navya2000v@gmail.com>
2024-12-07 19:39:37 -0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const url = require('url');
-const querystring = require('querystring');
 
 // MongoDB connection URI and client setup
 const uri = 'mongodb+srv://navya2000v:5QYtY04Ch02p73Qb@plant.dexcs.mongodb.net/plantDB?retryWrites=true&w=majority';
@@ -19,25 +18,6 @@ async function connectToDb() {
 }
 
 connectToDb();
-
-// Insert plant data into the database if not present
-async function insertPlants() {
-  try {
-    const db = dbClient.db('plantDB');
-    const plantsCollection = db.collection('plants');
-
-    const existingPlants = await plantsCollection.countDocuments();
-    if (existingPlants === 0) {
-      await plantsCollection.insertMany(plants);
-      console.log('Plants inserted into the database!');
-    } else {
-      console.log('Plants already exist in the database!');
-    }
-  } catch (err) {
-    console.error('Error inserting plants:', err);
-  }
-}
-insertPlants();
 
 // Utility function for sending JSON responses
 function sendJsonResponse(res, statusCode, data) {
@@ -61,42 +41,29 @@ const server = http.createServer(async (req, res) => {
     return;
   }
 
-  // Get plant details by name from query parameter
+  // Get all plants data
   if (method === 'GET' && parsedUrl.pathname === '/api') {
-    const query = parsedUrl.query;
-    const plantName = query.name;
-
-    if (!plantName) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Plant name query parameter is required' }));
-      return;
-    }
-
     try {
       const db = dbClient.db('plantDB');
       const plantsCollection = db.collection('plants');
-      const plant = await plantsCollection.findOne({ name: plantName });
+      const plants = await plantsCollection.find({}).toArray();
 
-      if (plant) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(plant));
+      if (plants.length > 0) {
+        sendJsonResponse(res, 200, plants); // Return all plants
       } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Plant not found' }));
+        sendJsonResponse(res, 404, { message: 'No plants found' });
       }
     } catch (err) {
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: err.message }));
+      sendJsonResponse(res, 500, { message: err.message });
     }
 
     return;
   }
 
-  // Default route
-  res.writeHead(404, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Route not found' }));
+  // Default route (if no matching route)
+  sendJsonResponse(res, 404, { message: 'Route not found' });
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
